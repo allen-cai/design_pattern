2cfdab9a6f361e98e8214bbb6a5822cbc2e50cab
zernan00 <cl30240@163.com>
2019-05-25 10:14:22 +0800

    简单工厂

---
+++ src/main/java/net/cailun/design/creational/simplefactory/Video.java
@@ -0,0 +1,12 @@
+package net.cailun.design.creational.simplefactory;
+
+/**
+ * @ClassName ${className}
+ * @Description TODO
+ * @Author allen
+ * @Date: ${Date}
+ * @Version 1.0
+ */
+public abstract class Video {
+    public abstract void produce();
+}
